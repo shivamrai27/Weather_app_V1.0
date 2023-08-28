643ee967a34a02d0d2fc0e0d51605df07f6b2302
Shivam Rai <200shivamrai@gmail.com>
2023-08-29 03:31:52 +0530

    Weather App V1.0

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 const apikey = '767c8ffcf3a981f284db98dfa3e28d58';
-
 const weatherDataEl = document.getElementById('weather-data');
-
 const inputTextEl = document.getElementById('input_text');
-
 const formEl = document.querySelector('form');
 
 formEl.addEventListener('submit', (event) => {
     event.preventDefault();
-
     const cityValue = inputTextEl.value;
-
     getWeatherData(cityValue);
-
 })
 
 async function getWeatherData(cityValue) {
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metrics`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`)
         if (!response.ok) {
             throw new Error("Network response was not ok")
         }
@@ -26,12 +20,27 @@ async function getWeatherData(cityValue) {
         const description = data.weather[0].description;
         const icon = data.weather[0].icon;
         const details = [
-            `Feels like: ${Math.round(data.main.feels_like)}`,
-            `Humidity: ${Math.round(data.main.humidity)}`,
-            `Wind speed: ${Math.round(data.main.wind)}`
+            `${Math.round(data.main.feels_like)}° C`,
+            `${Math.round(data.main.humidity)}%`,
+            `${Math.round(data.wind.speed)} m/s`
         ]
+
+        weatherDataEl.querySelector(".icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="icon">`
+
+        // weatherDataEl.querySelector('.detailes').innerHTML = details
+        //     .map((detail) => `<section>${detail}</section>`)
+        //     .join("")
+
+        weatherDataEl.querySelector(".temp").textContent = `${temperature}° C`
+        weatherDataEl.querySelector(".description").textContent = `${description}`
+        weatherDataEl.querySelector(".feels-like").textContent = `${details[0]}`
+        weatherDataEl.querySelector(".humidity").textContent = `${details[1]}`
+        weatherDataEl.querySelector(".wind-speed").textContent = `${details[2]}`
+        document.getElementById("weather-data").style.display = "block";
+        document.getElementById("error").style.display = "none";
         console.log(data);
     } catch (error) {
-
+        document.getElementById("error").style.display = "block";
+        document.getElementById("weather-data").style.display = "none";
     }
 }
