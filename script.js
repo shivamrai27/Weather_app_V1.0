a2d3ec3f76f728d6f9517afe5dee987b46aec7f5
Shivam Rai <200shivamrai@gmail.com>
2023-08-28 23:58:08 +0530

    Weather App V1.0

--- script.js
+++ script.js
@@ -22,7 +22,15 @@ async function getWeatherData(cityValue) {
             throw new Error("Network response was not ok")
         }
         const data = await response.json();
-
+        const temperature = Math.round(data.main.temp);
+        const description = data.weather[0].description;
+        const icon = data.weather[0].icon;
+        const details = [
+            `Feels like: ${Math.round(data.main.feels_like)}`,
+            `Humidity: ${Math.round(data.main.humidity)}`,
+            `Wind speed: ${Math.round(data.main.wind)}`
+        ]
+        console.log(data);
     } catch (error) {
 
     }
